01fec4623c8ba64f3aa1b1ff38ae4e6ba7409f47
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2026-08-31 09:48:07 +0700

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { logos, socialMediaUrl } from "../Details";
+import { socialMediaUrl } from "../Details";
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
